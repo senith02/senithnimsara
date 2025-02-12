0c451b1d92083f8b260bbbcda14a87f5dfe6fb30
SenithNimsara <senith2002n@gmail.com>
2025-02-12 13:52:43 +0530

    make projects section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
                                                 <div className='flex flex-wrap gap-2'>
                                                         {frontendSkills.map((tech, key) => (
                                                                 <span key={key} className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
-                                                                hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition'>
+                                                                hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'>
                                                                         {tech}
                                                                 </span>
                                                         ))}
@@ -50,7 +50,7 @@ const About = () => {
                                                 <div className='flex flex-wrap gap-2'>
                                                         {backendSkills.map((tech, key) => (
                                                                 <span key={key} className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
-                                                                hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition'>
+                                                                hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'>
                                                                         {tech}
                                                                 </span>
                                                         ))}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,77 @@ const Projects = () => {
                         text-transparent text-center'>
                                 {" "}
                                 Projects
-                        </h2>
+          </h2>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+            <div className='p-6 rounded-xl border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'>
+              <h3 className='text-xl font-bold mb-2'>Hotel Mangement System</h3>
+              <p className='text-gray-400 mb-4'>Developed a Hotel Booking System using a microservices architecture for managing hotel information,
+                bookings, user reviews, and user profiles. Integrated Eureka for service discovery and Spring Cloud Gateway
+                for centralized API routing.</p>
+                <div className='flex flex-wrap gap-2 mb-4'>
+                  {["React","Node.js","SpringBoot","Mysql","Java"].map((tech, key) => (
+                  <span key={key} className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                  hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'>
+                          {tech}
+                  </span>
+                ))}
+                </div>
+                <div className='flex justify-between items-center'>
+                  <a href='#' className='text-blue-400 hover:text-blue-300 transition-colors my-4'> View Project</a>
+                </div>
+            </div>
+            <div className='p-6 rounded-xl border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'>
+              <h3 className='text-xl font-bold mb-2'>Hotel Mangement System</h3>
+              <p className='text-gray-400 mb-4'>Developed a Hotel Booking System using a microservices architecture for managing hotel information,
+                bookings, user reviews, and user profiles. Integrated Eureka for service discovery and Spring Cloud Gateway
+                for centralized API routing.</p>
+                <div className='flex flex-wrap gap-2 mb-4'>
+                  {["React","Node.js","SpringBoot","Mysql","Java"].map((tech, key) => (
+                  <span key={key} className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                  hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'>
+                          {tech}
+                  </span>
+                ))}
+                </div>
+                <div className='flex justify-between items-center'>
+                  <a href='#' className='text-blue-400 hover:text-blue-300 transition-colors my-4'> View Project</a>
+                </div>
+            </div>
+            <div className='p-6 rounded-xl border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'>
+              <h3 className='text-xl font-bold mb-2'>Hotel Mangement System</h3>
+              <p className='text-gray-400 mb-4'>Developed a Hotel Booking System using a microservices architecture for managing hotel information,
+                bookings, user reviews, and user profiles. Integrated Eureka for service discovery and Spring Cloud Gateway
+                for centralized API routing.</p>
+                <div className='flex flex-wrap gap-2 mb-4'>
+                  {["React","Node.js","SpringBoot","Mysql","Java"].map((tech, key) => (
+                  <span key={key} className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                  hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'>
+                          {tech}
+                  </span>
+                ))}
+                </div>
+                <div className='flex justify-between items-center'>
+                  <a href='#' className='text-blue-400 hover:text-blue-300 transition-colors my-4'> View Project</a>
+                </div>
+            </div>
+            <div className='p-6 rounded-xl border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'>
+              <h3 className='text-xl font-bold mb-2'>Hotel Mangement System</h3>
+              <p className='text-gray-400 mb-4'>Developed a Hotel Booking System using a microservices architecture for managing hotel information,
+                bookings, user reviews, and user profiles. Integrated Eureka for service discovery and Spring Cloud Gateway
+                for centralized API routing.</p>
+                <div className='flex flex-wrap gap-2 mb-4'>
+                  {["React","Node.js","SpringBoot","Mysql","Java"].map((tech, key) => (
+                  <span key={key} className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                  hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'>
+                          {tech}
+                  </span>
+                ))}
+                </div>
+                <div className='flex justify-between items-center'>
+                  <a href='#' className='text-blue-400 hover:text-blue-300 transition-colors my-4'> View Project</a>
+                </div>
+            </div>
+          </div>
         </div>
     </section>
   )
